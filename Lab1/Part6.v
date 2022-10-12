1738b671eb8085d8c5b016504305b5690da8ea30
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2022-10-12 11:46:36 +0700

    Fixed bug of Part6

--- Lab1/Part6.v
+++ Lab1/Part6.v
@@ -39,8 +39,8 @@ module mux_2bit_8to1 (S, Q, T, U, V, W, X, Y, Z, M);
 	
 	wire [1:0] mux0_out, mux1_out;
 	
-	assign M[0] = (~S[1] & mux0_out[0]) | (S[1] & mux1_out[0]);
-	assign M[1] = (~S[1] & mux0_out[1]) | (S[1] & mux1_out[1]);
+	assign M[0] = (~S[2] & mux0_out[0]) | (S[2] & mux1_out[0]);
+	assign M[1] = (~S[2] & mux0_out[1]) | (S[2] & mux1_out[1]);
 	
 	mux_2bit_4to1 mux0 (S[1:0], Q, T, U, V, mux0_out);
 	mux_2bit_4to1 mux1 (S[1:0], W, X, Y, Z, mux1_out);
